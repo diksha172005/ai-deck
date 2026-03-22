d5e2650984aa8883c5513cbd1f92c08887be456f
diksha172005 <dikshatiwari1723@gmail.com>
2026-03-22 21:56:44 +0530

    Add @Transactional annotation to FavoriteService

--- backend/src/main/java/com/aideckapp/service/FavoriteService.java
+++ backend/src/main/java/com/aideckapp/service/FavoriteService.java
@@ -7,11 +7,13 @@ import com.aideckapp.repository.ToolRepository;
 import com.aideckapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class FavoriteService {
 
     @Autowired
