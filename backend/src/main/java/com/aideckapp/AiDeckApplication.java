e14782fc3c660425465cb11e495f29f0d340661a
diksha172005 <dikshatiwari1723@gmail.com>
2026-04-13 16:14:28 +0530

    Enable asynchronous processing in AiDeckApplication

--- backend/src/main/java/com/aideckapp/AiDeckApplication.java
+++ backend/src/main/java/com/aideckapp/AiDeckApplication.java
@@ -2,8 +2,10 @@ package com.aideckapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AiDeckApplication {
     public static void main(String[] args) {
         SpringApplication.run(AiDeckApplication.class, args);
